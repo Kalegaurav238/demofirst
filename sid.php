9aab4f613fe51937c94626b21b121ebb5d2cbd96
gauravkale238 <gauravkale238@gmail.com>
2022-02-25 13:28:27 +0530

    first commit

---
+++ sid.php
@@ -0,0 +1,21 @@
+<?php
+echo '<html>';
+echo '<head>';
+echo '<title>';
+echo 'My website';
+echo '</title>';
+echo '</head>';
+echo '<body>';
+echo '<img src="Wular_Lake.jpg">';
+echo '<font color="yellow" size=18>';
+echo '<br>Wular lake</br>';
+echo '</font>';
+echo '<br>Wular Lake is one of the largest fresh water lakes in Asia and the largest in India, is located in Jammu and Kashmir. </br>';
+echo '<br><i>It is measures 24 kms across and surrounded by towering mountains, The green water of the Wular Lake';
+echo 'is an important natural habitat for fish, a rich population of birds and wildlife. </br></i>';
+echo '<br><li>Thousands of people living on its shoresand elsewhere in the Kashmir valley depend on Wular Lake for fishing to earn their livelihood. </li></br>';
+echo 'The Kashmir valley has few more beautiful lakes like Nagin Lake and most popular Dal Lake.';
+echo '<a href="sid1.html">show the Wular Lake</a>';
+echo '</body>';
+echo '</html>';
+?>
